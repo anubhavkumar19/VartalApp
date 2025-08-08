2f37f64975bec0579f84ec860aaf3dc0f5cc6516
Anubhav Kumar <anubhavkumar22166@gmail.com>
2025-08-08 12:56:25 +0530

    add backend logic for user signup with validation and database integration

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,49 @@
-export const signup = (req,res) => {
-    res.send("signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bycrypt from "bcryptjs";
+
+export const signup = async (req,res) => {
+    const {fullname, email,password}= req.body;
+    try {
+        if(!fullname || !email || !password){
+            return res.status(400).json({message: "All fields are required!"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be atleast 6 chracters!"});
+        }
+
+        const user = await User.findOne({email})
+
+        if(user) return res.status(400).json({message:"Email already exists!"});
+
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(password,salt);
+
+        const newUser = User({
+            fullname,
+            email,
+            password:hashedPassword
+        });
+
+        if(newUser){
+            //generate jwt token here
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        }else{
+            return res.status(400).json({message: "Invalid data!"});
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message: "Internal Server Error!"});
+    }
 };
 
 export const login = (req,res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import { connectDb } from "./lib/db.js"
+import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./routes/auth.routes.js";
 
@@ -10,9 +10,11 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT: " + PORT);
-    connectDb();
+    connectDB();
 })
